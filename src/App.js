61c383d2ecd5be9abc5381acdfde2325bd3d7c2d
isupersky <aakash9868sinha@gmail.com>
2020-11-07 15:53:25 +0530

    pipeline test 6

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           Coming Soon with your nearest Kirana Stores.
         </p>
         <Button variant="outlined" color="primary">
-          aakash
+           Shivani is the best
         </Button>  
     </header>
     </div>
