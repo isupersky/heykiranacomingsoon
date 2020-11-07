618ea5afde40887119d532b24a46a2f592c00c6c
isupersky <aakash9868sinha@gmail.com>
2020-11-07 18:15:18 +0530

    added docker file

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           Coming Soon with your nearest Kirana Stores.
         </p>
         <Button variant="outlined" color="primary">
-           Shivani is the best
+          Get Added to the Wait List
         </Button>  
     </header>
     </div>
