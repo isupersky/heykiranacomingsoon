83ef5dcd2538f89ebf19e4af87a632127d7a115b
iAakashSinha <aakash.sinha@tothenew.com>
2020-11-07 12:36:53 +0530

    first Draft

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
+import logo from './images/logo.png';
 import './App.css';
+import Button from '@material-ui/core/Button';
+
 
 function App() {
   return (
@@ -7,17 +9,12 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Coming Soon with your nearest Kirana Stores.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button variant="outlined" color="primary">
+          Get Added to the Wait List
+        </Button>  
+    </header>
     </div>
   );
 }
---
+++ src/content.js
@@ -0,0 +1,11 @@
+// import React  from 'react';
+// import './content.css';
+// import Button from '@material-ui/core/Button';
+
+
+// const content =()=>{
+//   <Button variant="outlined" color="primary">
+//     Get Added to the Wait List
+//   </Button>  
+    
+//   }
