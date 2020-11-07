4855abee794d3568f13912ea6a7ea5cfe45bdd87
isupersky <aakash9868sinha@gmail.com>
2020-11-07 15:22:28 +0530

    pipeline test4

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           Coming Soon with your nearest Kirana Stores.
         </p>
         <Button variant="outlined" color="primary">
-          Thank you CDO/E
+          sahdkshdsdhskhdk
         </Button>  
     </header>
     </div>
